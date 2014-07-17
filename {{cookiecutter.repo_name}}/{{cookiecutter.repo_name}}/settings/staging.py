3c4fc0c0f1f7229e84f40944bb6b520b37e6d9e4
Lucian Ursu <lucian.ursu@gmail.com>
2014-07-17 17:51:29 +0300

    Filled in django staging and production settings.

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/staging.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/staging.py
@@ -1,3 +1,19 @@
 from {{ repo_name }}.settings.common import *
 
 DEBUG = False
+
+DATABASES['default'].update({
+    'NAME': '{{ repo_name }}',
+    'USER': '{{ repo_name }}',
+})
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': '127.0.0.1:11211',
+    },
+}
+
+ALLOWED_HOSTS = [
+    '.{{ domain_name }}',
+]
