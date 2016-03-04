53ffa6f5aaa7404f8d153a66d1d0fbf805cd4596
Lucian Ursu <lucian.ursu@gmail.com>
2016-03-04 17:05:11 +0200

    Added an ADMIN_URL setting and included the URL in the global urls.

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_URL = 'account_login'
+ADMIN_URL = r'^admin/'
 
 # Authentication
 AUTHENTICATION_BACKENDS = (
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.contrib import admin
 
 
 urlpatterns = [
+    url(settings.ADMIN_URL, include(admin.site.urls)),
     url(r'^$', '{{ cookiecutter.repo_name }}.views.under_construction',
         name='under_construction'),
 ]
