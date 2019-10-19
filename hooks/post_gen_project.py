9ff1f47887570fa036cd804bf0a0c5c39b31c081
Bogdan <bogdanionutganea@gmail.com>
2019-10-19 20:21:50 +0300

    option for GIS and Non GIS projects

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,22 @@
 """
 Generates a Python file with the secret key needed by Django.
 """
-
-import os
-import random
-
+# import os
+# import random
+# import shutil
+# import string
+# def remove_docker_files():
+#     shutil.rmtree("compose")
+#
+#     file_names = ["local.yml", "production.yml", ".dockerignore"]
+#     for file_name in file_names:
+#         os.remove(file_name)
+#
+#
+# def remove_utility_files():
+#     shutil.rmtree("utility")
+#
+# if "{{ cookiecutter.GIS_project }}".lower() == "y":
+#         remove_utility_files()
+#     else:
+#         remove_docker_files()
