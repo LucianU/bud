a902c8bc6bf57072df31dbca7ad5be3b0e6383cb
Lucian Ursu <lucian.ursu@gmail.com>
2020-02-26 12:28:36 +0200

    Update urls.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -1,4 +1,4 @@
-"""{{cookiecutter.project_slug}} URL Configuration
+"""bull URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,15 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('{{cookiecutter.project_slug}}.apps.{{cookiecutter.app_name}}.urls')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
