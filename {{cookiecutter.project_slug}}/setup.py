11203f98d407a9355b73234a5da3b430be6de9a6
Lucian Ursu <lucian.ursu@gmail.com>
2025-03-11 14:23:52 +0200

    Remove setup.py
    Not needed with poetry.

--- {{cookiecutter.project_slug}}/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(name='{{cookiecutter.project_slug}}',
-      version='0.1',
-      packages=find_packages(),
-      include_package_data=True,
-      scripts=["manage.py"]
-)
